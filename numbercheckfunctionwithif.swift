ae96db885b0471de38d2eecd9cd138f82198c4e1
Arjun Sharda <77706434+ajsharda17@users.noreply.github.com>
2022-06-09 09:52:37 -0500

    Create numbercheckfunctionwithif.swift

---
+++ numbercheckfunctionwithif.swift
@@ -0,0 +1,13 @@
+func numcheckfunction(_ var1: Int,_ var2: Int) {
+  if var1 > var2 {
+    print("Var1 is greater than Var2! \n Var1: \(var1) \n Var2: \(var2)")
+  }
+  if var2 > var1 {
+    print("Var2 is greater than Var1! \n Var1: \(var1) \n Var2: \(var2)")
+  }
+  if var1 == var2 {
+    print("Var1 and Var2 are equal! \n Var1: \(var1) \n Var2: \(var2)")
+  }
+  
+}
+numcheckfunction(10, 20)
